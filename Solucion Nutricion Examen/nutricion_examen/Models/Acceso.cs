0551d78b447db257f1a0035f41649a03399f8222
mvallepastene <66449663+mvallepastene@users.noreply.github.com>
2020-06-12 21:35:06 -0400

    Creacion modelos
    Se crean modelos de entidades, Acceso, E R24h, ficha paciente y nutricionista.

---
+++ Solucion Nutricion Examen/nutricion_examen/Models/Acceso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nutricion_examen.Models
+{
+    public class Acceso
+    {
+        public int Id_Acceso { get; set; }
+        public string Usuario { get; set; }
+        public string Pass { get; set; }
+        public char Tipo_Usuario { get; set; }
+    }
+}
---
+++ Solucion Nutricion Examen/nutricion_examen/Models/Ficha_Medica_Paciente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nutricion_examen.Models
+{
+    public class Ficha_Medica_Paciente
+    {
+        public int Id_Ficha { get; set; }
+        public string Ant_Familiares { get; set; }
+        public string Alergias { get; set; }
+        public string Enfermedades { get; set; }
+        public float Peso { get; set; }
+        public float Talla { get; set; }
+        public float Imc { get; set; }
+        public string Diagnostico { get; set; }
+        public DateTime Proximo_Control { get; set; }
+        public DateTime Fecha_Creacion_Ficha { get; set; }
+        public int Id_Nutricionista { get; set; }
+        public int Id_Paciente { get; set; }
+    }
+}
