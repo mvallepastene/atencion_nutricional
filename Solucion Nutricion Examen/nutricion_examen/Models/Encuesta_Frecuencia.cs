71bc278fd742f6759ebb6404d9c93907f3f8b1bf
Kemberlin577 <66705233+Kemberlin577@users.noreply.github.com>
2020-06-12 22:34:33 -0400

    Cambios Kem
    Se agregaron 3 modelos
    Agenda, encuesta Frecuencia, Paciente

---
+++ Solucion Nutricion Examen/nutricion_examen/Models/Encuesta_Frecuencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nutricion_examen.Models
+{
+    public class Encuesta_Frecuencia
+    {
+        public int Id_Frecuencia { get; set; }
+        public string Alimentos { get; set; }
+        public string Frecuencia { get; set; }
+        public int Cantidad_Gr_Cc { get; set; }
+        public string Observaciones { get; set; }
+        public int Id_Ficha { get; set; }
+    }
+}
