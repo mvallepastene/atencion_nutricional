9c073f3d1568111e85bda49aae8f24d037a91ca6
Kemberlin577 <66705233+Kemberlin577@users.noreply.github.com>
2020-06-19 17:04:34 -0400

    Últimas actualizaciones KEM

---
+++ Solucion Nutricion Examen/nutricion_examen/Controllers/AgendaController.cs
@@ -0,0 +1,94 @@
+﻿using nutricion_examen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+
+namespace nutricion_examen.Controllers
+{
+    public class AgendaController : Controller
+    {
+        // GET: Agenda
+        public ActionResult Index()
+        {
+            //hacemos la peticion a la bd
+            var result = DapperORM.ReturnList<Agenda>("sp_traer_Agenda");
+            return Json(new {data = result }, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Agenda/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Agenda/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agenda/Create
+        [HttpPost]
+        public ActionResult Create(Agenda agenda)
+        {
+
+            // TODO: Add insert logic here
+
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@id_agenda", agenda.Id_Agenda);
+            
+            DapperORM.ExecuteWithoutReturn("sp_agregar_actualizar_Paciente", param);
+
+
+            return RedirectToAction("Index");
+          
+        }
+
+        // GET: Agenda/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Agenda/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Agenda/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Agenda/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
