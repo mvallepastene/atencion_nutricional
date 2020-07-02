33794c08dc57062c2f2756bdf80c3c338b658e1b
Mario Gutierrez <mario@turisclub.cl>
2020-07-02 01:48:21 -0400

    Adicion del Login para acceder al Sisitema

--- Solucion Nutricion Examen/nutricion_examen/Controllers/HomeController.cs
+++ Solucion Nutricion Examen/nutricion_examen/Controllers/HomeController.cs
@@ -3,28 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using nutricion_examen.Models;
+using Dapper;
 
 namespace nutricion_examen.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult LogIn(string message = "")
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = message;
 
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult LogIn(string usr, string psw)
         {
-            ViewBag.Message = "Your contact page.";
+            if (!string.IsNullOrEmpty(usr) && !string.IsNullOrEmpty(psw))
+            {
+                DynamicParameters param = new DynamicParameters();
+                param.Add("@usr", usr);
+                param.Add("@psw", psw);
 
-            return View();
+                List<Acceso> _list = new List<Acceso>();
+                _list = DapperORM.ReturnList<Acceso>("sp_getUsuario", param).ToList();
+
+                var User = _list.Select(u => new Acceso
+                {
+                    Id_Acceso = u.Id_Acceso,
+                    Usuario = u.Usuario,
+                    Pass = u.Pass,
+                    Tipo_Usuario = u.Tipo_Usuario,
+                    
+                });
+                if (User != null)
+                {
+                    foreach (var item in User)
+                    {
+                        FormsAuthentication.SetAuthCookie(item.Usuario, true);
+                    }
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return RedirectToAction("LogIn", new { message = "No encontramos sus datos ingresados" });
+
+                }
+
+            }
+            else
+            {
+                return RedirectToAction("LogIn", new { message = "Llena los datos para iniciar Sesion" });
+
+            }
+
+        }
+
+        [Authorize]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
         }
     }
 }
